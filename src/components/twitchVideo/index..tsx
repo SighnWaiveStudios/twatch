2479e66908f823f93bc9317278d535d500ad8450
SighnWaive <sighnwaive@gmail.com>
2022-01-28 21:35:31 -0500

    Converted to Typescript

---
+++ src/components/twitchVideo/index..tsx
@@ -0,0 +1,12 @@
+
+export default function () {
+    return (
+        <iframe
+            src="https://player.twitch.tv/?<channel, video, or collection>&parent=streamernews.example.com"
+            height="400px"
+            width="400px"
+            allowFullScreen={true}>
+        </iframe>
+    )
+}
+
